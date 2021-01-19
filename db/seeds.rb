188a81565369221976081a383e6e99692a739d6f
Glenn Bryant <glennbryant3@gmail.com>
2021-01-19 20:16:50 +0900

    Latest version running

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding..."
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Cocktail.create(name:"The Lupita")
+Dose.create(description: "4 cl")
+puts "Seeding Complete!"
